5df3627739dcb654445c1cdf5d7eced3112a3efa
Tia Rose <shortyshort789@aol.com>
2019-06-19 14:26:36 -0700

    have message from app.js showing in logger.js

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 'use strict';
 
-
 const alter = require('./mod/alter');
 
-const net = require('net');
-const client = new net.Socket();
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
+const faker = require('faker');
+
 
 const alterFile = (file) => {
-  // fs.readFile( file, (err, data) => {
-  //   if(err) { throw err; }
-  //   let text = data.toString().toUpperCase();
-  //   fs.writeFile( file, Buffer.from(text), (err, data) => {
-  //     if(err) { throw err; }
-  //     console.log(`${file} saved`);
-  //   });
-  // });
-
+ 
   alter.readFile(file)
     .then(data => {
       alter.writeFile(file, alter.upper(data));
+      socket.emit('speak', faker.random.words());
     })
     .catch(error => {
-      client.write(error);
+      socket.emit('error', error);
     });
 };
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(3000);
 
 io.on('connection', socket => {
   console.log(`Connected socket id: ${socket.id}`);
-  socket.on('speck', payload => {
+  socket.on('speak', payload => {
     socket.broadcast.emit('message', payload);
   });
 });
